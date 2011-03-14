6913d50f7e04fb14a3ab7357bd5de99b74681be6
Michael Steele <octoberclub@gmail.com>
2011-03-14 16:26:00 +0000

    Added PiezoTest2 with note frequency calculation

---
+++ Netduino.PiezoTest2/NoteFrequencies.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.SPOT;
+
+public static class NoteFrequencies
+{
+    /// <summary>
+    /// Static method to calculate the frequency of a note from its name and octave.
+    /// </summary>
+    /// <param name="octave">Octave number (0-7)</param>
+    /// <param name="note">Note to be found: eg. F#, A, Bb</param>
+    /// <returns></returns>
+    public static float CalculateFrequency(int octave, string note)
+    {
+        string noteLC = note.ToLower();
+        string[] notes = "c,c#,d,d#,e,f,f#,g,g#,a,a#,b".Split(',');
+
+        // loop through each note until we find the index of the one we want        
+        for (int n = 0; n < notes.Length; n++)
+        {
+            if (notes[n] == noteLC // frequency found for major and sharp notes
+                || (note.Length > 1 && noteLC[1] == 'b' && notes[n + 1][0] == noteLC[0])) // or flat of next note
+            {
+                // Multiply initial note by 2 to the power (n / 12) to get correct frequency, 
+                //  (where n is the number of notes above the first note). 
+                //  Then mutiply that value by 2 to go up each octave
+                return (16.35f * (float)System.Math.Pow(2, (n / 12)))
+                    * (float)System.Math.Pow(2, octave);
+            }
+        }
+        throw new ArgumentException("No frequency found for note : " + note, note);
+    }
+}
---
+++ Netduino.PiezoTest2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Netduino.PiezoTest
+{
+    /// <summary>
+    /// Program based on Ray Mckaig 's forum post 'Music with Piezo speaker' on Netduino website 13 Dec 2010
+    /// http://forums.netduino.com/index.php?/topic/831-music-with-a-piezo-speaker/page__p__6044__hl__piezo__fromsearch__1#entry6044
+    /// 
+    /// Video tutorial here:
+    /// http://www.youtube.com/watch?v=vEr4qk9lTEU
+    /// 
+    /// I've tried to put this together implementing some of the missing classes from the post.
+    /// 
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            Test_PiezoSpeaker();
+        }
+
+        //And finally, some test code that demonstrates using the above code to play music.
+
+        public static void Test_PiezoSpeaker()
+        {
+            // setup the speaker on digital pin 5
+            PiezoSpeaker speaker = new PiezoSpeaker(Pins.GPIO_PIN_D5);
+
+            // define some tunes
+            string[] tunes = new string[] 
+            {
+                "C * F / F * F A G / F * G A F / F A + C D" , // Auld Lang Syne
+                "D / D * * E D G * F# / / D / D * * E D A * G / / D / D * * + D - B G F# * E / / + C / C * * - B G A * G", // Happy birthday
+                "C C G G A A * G / F F E E D D * C / G G F F E E * D / G G F F E E * D / C C G G A A * G / F F E E D D * C" , // twinkle, twinkle
+                "A B + C D E F# G# A G F E D C - B A", // melodic minor scale                
+            };
+
+
+            // each time the button is pressed we're going to cycle to the next tune in the list
+            // if we reach the end of the list, start at the beginning again.
+
+            int which = -1;
+
+            Button button = new Button(Pins.ONBOARD_SW1);
+            button.Pressed += delegate
+            {
+                which++;
+
+                if (which == tunes.Length)
+                    which = 0;
+
+                speaker.Play(tunes[which], 4, 333);
+            };
+
+            // as this is driven by the button being pressed, we don't need to poll
+            // anything so we just put the main thread to sleep and wait... and wait... and wait...
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
